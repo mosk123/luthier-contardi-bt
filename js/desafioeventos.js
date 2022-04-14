18a81caa5a7e62cf219dc0679998a45ea8631ae3
Mosky <mosky66@gmail.com>
2022-04-14 16:23:48 -0300

    correcion codigo

--- js/desafioeventos.js
+++ js/desafioeventos.js
@@ -59,13 +59,14 @@ renderizarTienda(JSON.parse(localStorage.getItem('guitarras')))
 /* Boton Carrito */
 
 const clickBtn = document.querySelectorAll(".button");
-const tbody = document.getElementsByName('tbody');
+const tbody = document.getElementById('tbody');
 const carrito = []
 
 clickBtn.forEach(btn =>{
   btn.addEventListener("click", (agregarAlCarrito))
 })
 
+/* Agregar al carrito card */
 
 function agregarAlCarrito(e){
   const button = e.target
@@ -81,6 +82,7 @@ function agregarAlCarrito(e){
   }
   agregarItemCarrito(nuevoItem)
 }
+/* PUshear al carrito */
 
 function agregarItemCarrito(nuevoItem){
 
@@ -89,14 +91,16 @@ carrito.push(nuevoItem)
 renderCarrito();
 }
 
+/* Renderiza carrito */
+
 function renderCarrito(){
 
-tbody.innerHTML = ''
+
 carrito.map(item => {
    const tr = document.createElement("tr")
    tr.classList.add("itemCarrito")
 
-   const Content = `
+   tr.innerHTML = `
    <th scope="row">1</th>
    <td class="table__productos">
    <h6 class="title">${item.title}</h6>
@@ -107,9 +111,7 @@ carrito.map(item => {
    <button class="delete btn btn-danger">x</button>
    /td>
    `
-
-  tr.innerHTML = Content;
-  tbody.append(tr);
+  tr.append(tbody);
 
   tr.querySelector(".delete").addEventListener('click', removeItemCarrito)
   tr.querySelector(".input__elemento").addEventListener('change', sumaCantidad)
@@ -117,6 +119,73 @@ carrito.map(item => {
 CarritoTotal()
 }
 
+/* boton carrito */
+
+let boton = document.getElementById("btnCarrito")
+boton.onclick = () => {
+  document.getElementsByTagName("h4").className.remove("carritoTable")
+}
+
+
+
+function CarritoTotal(){
+  let Total = 0;
+  const itemCartTotal = document.querySelector('.itemCartTotal')
+  carrito.forEach((item) => {
+    const precio = Number(item.precio.replace("$", ''))
+    Total = Total + precio*item.cantidad
+  })
+
+  itemCartTotal.innerHTML = `Total ${Total}`;
+  addLocalStorage()
+}
+
+function removeItemCarrito(e){
+  const buttonDelete = e.target
+  const tr = buttonDelete.closest(".ItemCarrito")
+  const title = tr.querySelector('.title').textContent;
+  for(let i=0; i<carrito.length ; i++){
+
+    if(carrito[i].title.trim() === title.trim()){
+      carrito.splice(i, 1)
+    }
+  }
+
+  const alert = document.querySelector('.remove')
+
+  setTimeout( function(){
+    alert.classList.add('remove')
+  }, 2000)
+    alert.classList.remove('remove')
+
+  tr.remove()
+  CarritoTotal()
+}
+
+function sumaCantidad(e){
+  const sumaInput  = e.target
+  const tr = sumaInput.closest(".ItemCarrito")
+  const title = tr.querySelector('.title').textContent;
+  carrito.forEach(item => {
+    if(item.title.trim() === title){
+      sumaInput.value < 1 ?  (sumaInput.value = 1) : sumaInput.value;
+      item.cantidad = sumaInput.value;
+      CarritoTotal()
+    }
+  })
+}
+
+function addLocalStorage(){
+  localStorage.setItem('carrito', JSON.stringify(carrito))
+}
+
+/* window.onload = function(){
+  const storage = JSON.parse(localStorage.getItem('carrito'));
+  if(storage){
+    carrito = storage;
+    renderCarrito()
+  }
+} */
 
 
 
